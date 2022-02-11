f1aca655e594a10c075a065914efa616d0a8aaf2
CucerOriginal <87267507+CucerOriginal@users.noreply.github.com>
2022-02-11 23:42:52 +0300

    Добавьте файлы проекта.

---
+++ AppZadanie1/AppZadanie1/ViewModel/ViewModel1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using AppZadanie1;
+using AppZadanie1.VIews;
+using Xamarin.Forms;
+
+namespace AppZadanie1.ViewModel
+{
+    public class ViewModel1 : INotifyPropertyChanged
+    {
+        string number;
+        int a = 1000;
+        
+        
+        public ViewModel1()
+        { 
+            
+
+            ChangeText = new Command(() =>
+            {
+                a -= 7;
+                Number = Number + Convert.ToString(a) + " ";
+            }
+            );
+        }
+        public ICommand NewPage
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await Application.Current.MainPage.Navigation.PushAsync(new Page2());
+                });
+            }
+        }
+
+        
+        public string Number
+        {
+            get => number;
+            set
+            {
+                number = value;
+
+                var args = new PropertyChangedEventArgs(nameof(Number));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public Command ChangeText { get; }
+    }
+}
---
+++ AppZadanie1/AppZadanie1/ViewModel/ViewModel2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AppZadanie1.ViewModel
+{
+    public class ViewModel2 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
